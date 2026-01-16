74e3ca1a4effc59ce747f041561976313fe66b20
roilastorban <roilastorban@gmail.com>
2026-01-16 21:12:29 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
-  // Ajoute ces deux sections ci-dessous :
+  devIndicators: {
+    buildActivity: false,
+  },
   eslint: {
-    // Permet de terminer le build même s'il y a des erreurs d'apostrophes
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Permet de terminer le build même s'il y a des erreurs de types (ex: any)
     ignoreBuildErrors: true,
   },
 };
 
 export default nextConfig;
-
